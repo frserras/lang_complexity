d3010055de0d2d9bb789f8af2603b1c2ff07863d
frserras <49004258+frserras@users.noreply.github.com>
2026-03-17 15:50:34 +0000

    refactor test_unit.py

--- test/test_unit.py
+++ test/test_unit.py
@@ -1,34 +1,38 @@
 import unittest
-from random import random
 import unit as U
 
-
 class TestUnit(unittest.TestCase):
-    def test_Chars(self):
-        s = "abcdefghijklmnopqrstuvwxz"
-        c = U.Chars()
-        result = c.parse(s)
+    
+    def test_chars(self):
+        s = "abcdefghijklmnopqrstuvwxyz"
+        parser = U.Chars()
+        
+        result = parser.parse(s)
+        joined = result.reconstruct()
+        
         self.assertEqual(len(result.index), len(s))
-        joined = result.reconstruct()
         self.assertEqual(len(joined), len(s))
         self.assertEqual(joined, s)
 
-    def test_NonSpaces(self):
-        s = " ".join("abcdefghijklmnopqrstuvwxz")
-        ns = U.NotChar(" ")
-        result = ns.parse(s)
+    def test_non_spaces(self):
+        s = " ".join("abcdefghijklmnopqrstuvwxyz")
+        parser = U.NotChar(" ")
+        
+        result = parser.parse(s)
         joined = result.reconstruct()
+
+        self.assertEqual(len(joined), len(s))
+        self.assertEqual(joined, s) 
+
+    def test_lines(self):
+        s = "\n".join("abcdefghijklmnopqrstuvwxyz")
+        parser = U.NotChar("\n")
+        
+        result = parser.parse(s)
+        joined = result.reconstruct()
+        
         self.assertEqual(len(joined), len(s))
         self.assertEqual(joined, s)
-
-    def test_Lines(self):
-        s = "\n".join("abcdefghijklmnopqrstuvwxz")
-        l = U.NotChar("\n")
-        result = l.parse(s)
-        joined = result.reconstruct()
-        self.assertEqual(len(joined), len(s))
-        self.assertEqual(joined, s)
-
 
 if __name__ == "__main__":
     unittest.main()
