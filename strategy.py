9d732cf79481dc3d24bf5f7259a45932370e6ff4
frserras <49004258+frserras@users.noreply.github.com>
2026-03-26 18:24:08 +0000

    Update CharMasking Strategy to a general Masking Strategy for both words and chars
    Co-authored-by: Rui Emanuel Vieira <64679344+E-RuiL-Vieira@users.noreply.github.com>
    Co-authored-by: gabrielfsouza2002 <gabrielfsouza.araujo@usp.br>
    Co-authored-by: lirineu <54121708+lirineu@users.noreply.github.com>

--- strategy.py
+++ strategy.py
@@ -1,6 +1,7 @@
 import random
 from unit import ParseResult
 from abc import ABC, abstractmethod
+import re
 
 
 class Strategy(ABC):
@@ -42,24 +43,24 @@ class Replacement(Strategy):
         )
         return output
     
-class CharMasking(Strategy):
+class Masking(Strategy):
     def __init__(self, percent: float, seed=None, mask='α'):
         super().__init__()
         self.percent = percent
         self.rng = random.Random(seed)
         self.mask = mask
     
-    def _mask_and_reconstruct(self, chars, indices_to_mask):
+    def _mask_and_reconstruct(self, units, indices_to_mask):
         for idx in indices_to_mask:
-            chars[idx] = self.mask
-        chars_masked = ''.join(chars)
-        return chars_masked
+            units[idx] = len(units[idx]) * self.mask
+        seq_masked = ''.join(units)
+        return seq_masked
 
     def execute(self, presult: ParseResult) -> str:
-        values, chars = list(presult.index), list(presult.sequence)
-        num_to_replace = int(len(values) * self.percent)
-        indices_to_mask = self.rng.sample(values, num_to_replace)
-        output = self._mask_and_reconstruct(chars, indices_to_mask)
+        indices, units = list(presult.index), list(presult.sequence)
+        num_to_replace = int(len(indices) * self.percent)
+        indices_to_mask = self.rng.sample(indices, num_to_replace)
+        output = self._mask_and_reconstruct(units, indices_to_mask)
         return output
 
 
